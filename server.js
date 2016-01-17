783a7bc3c4dcbda5fc2a7318588501d0aaf43025
elson lim <elsonlim7@gmail.com>
2016-01-17 22:33:01 +0800

    space

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var middleware = require('./middleware.js');
 
-
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
